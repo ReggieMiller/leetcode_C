4b499cac42afb22527d093211b453db3aeb6dd8f
ReggieMiller <506147884@qq.com>
2016-10-25 17:02:06 +0800

    Update functions.c

--- 3. Longest Substring Without Repeating Characters/functions.c
+++ 3. Longest Substring Without Repeating Characters/functions.c
@@ -1,1 +1,34 @@
-#include 
+int lengthOfLongestSubstring(char* s) {
+    int max = 0;
+    int curLong = 0;
+    int curPos = 0;
+    int prevPos = 0;
+    char* lastPos = 0;
+    char caSubstr[1024] = {0};
+    
+    while(s[curPos] != '\0')
+    {
+        memset(caSubstr,0,sizeof(caSubstr));
+        curLong = curPos-prevPos+1;   
+        if(curLong > max )
+        {
+            max = curLong;
+        }
+        
+        curPos++;
+        
+        memcpy(caSubstr,s+prevPos,curLong);
+        
+        lastPos = strchr(caSubstr,s[curPos]);
+
+        if(lastPos != 0)
+        {
+            prevPos = curPos-strlen(lastPos)+1;
+        }
+ 
+
+    }
+    return max;
+    
+    
+}
