fa76858caf827cf92ae0aa0be1f816002fc52471
ReggieMiller <506147884@qq.com>
2016-10-28 14:26:41 +0800

    Update functions.c

--- 1.TWO SUM/functions.c
+++ 1.TWO SUM/functions.c
@@ -2,9 +2,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target) {
-    int *result;
-    result = malloc(sizeof(char)*2);
-    //int count = 0;
+    int *result = malloc(sizeof(char)*2);
     for(int i = 0 ; i < numsSize-1 ; i++)
     {
         for(int j = i+1 ; j<numsSize; j++ )
